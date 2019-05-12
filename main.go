c87189a4d6547ba6d7009f14fac1f09a2942d92b
Jessica Greene <jessica.greene@ecosia.org>
2019-05-12 22:20:20 +0200

    create main.go for webserver

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+    http.HandleFunc("/", hello)
+    http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("hello!"))
+}
